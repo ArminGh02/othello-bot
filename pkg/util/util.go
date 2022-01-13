c446fdcd5860458260afcbbe80d4f1f0778b0ba4
ArminGh02 <arminghorbanian02@gmail.com>
2022-01-13 19:17:18 +0330

    Move `CoordSet` to util.go

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -4,3 +4,28 @@ type Coord struct {
 	X int
 	Y int
 }
+
+type CoordSet struct {
+	m map[Coord]struct{}
+}
+
+func NewCoordSet() CoordSet {
+	return CoordSet{
+		m: make(map[Coord]struct{}),
+	}
+}
+
+func (set *CoordSet) Clear() {
+	for key := range set.m {
+		delete(set.m, key)
+	}
+}
+
+func (set *CoordSet) Insert(c Coord) {
+	set.m[c] = struct{}{}
+}
+
+func (set *CoordSet) Contains(c Coord) bool {
+	_, present := set.m[c]
+	return present
+}
