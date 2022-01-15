723314e39e5ff77b7887193e41e8d49fc6ef28fe
ArminGh02 <arminghorbanian02@gmail.com>
2022-01-15 10:50:30 +0330

    Write `getEditedMsgOfGame` function

--- pkg/othellobot/util.go
+++ pkg/othellobot/util.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/ArminGh02/othello-bot/pkg/consts"
+	"github.com/ArminGh02/othello-bot/pkg/othellogame"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func getGameMsg(whiteUser, blackUser *tgbotapi.User, whiteDisks, blackDisks int) string {
-	return fmt.Sprintf("%s%s: %d\n%s%s: %d\nDon't count your chickens before they hatch!",
+func getGameMsg(activeUser, whiteUser, blackUser *tgbotapi.User, whiteDisks, blackDisks int) string {
+	return fmt.Sprintf("Turn of: %s\n%s%s: %d\n%s%s: %d\nDon't count your chickens before they hatch!",
+		activeUser.FirstName,
 		consts.WHITE_DISK_EMOJI,
 		whiteUser.FirstName,
 		whiteDisks,
@@ -16,6 +18,20 @@ func getGameMsg(whiteUser, blackUser *tgbotapi.User, whiteDisks, blackDisks int)
 		blackUser.FirstName,
 		blackDisks,
 	)
+}
+
+func getEditedMsgOfGame(inlineMessageID string, game *othellogame.Game) tgbotapi.EditMessageTextConfig {
+	return tgbotapi.EditMessageTextConfig{
+		BaseEdit: tgbotapi.BaseEdit{
+			InlineMessageID: inlineMessageID,
+			ReplyMarkup:     &tgbotapi.InlineKeyboardMarkup{InlineKeyboard: game.InlineKeyboard()},
+		},
+		Text: getGameMsg(
+			game.ActiveUser(),
+			game.WhiteUser(), game.BlackUser(),
+			game.WhiteDisks(), game.BlackDisks(),
+		),
+	}
 }
 
 func buildMainKeyboard() tgbotapi.ReplyKeyboardMarkup {
