e2f67ace1b7a3d7ebd74ed84df9c34b2cf90b1ed
ArminGh02 <arminghorbanian02@gmail.com>
2022-01-15 16:27:17 +0330

    Convert if-else to swtich-case

--- pkg/othellogame/game.go
+++ pkg/othellogame/game.go
@@ -205,9 +205,10 @@ func (g *Game) updateDisksCount() {
 	white, black := 0, 0
 	for _, row := range g.board {
 		for _, c := range row {
-			if c == cell.WHITE {
+			switch c {
+			case cell.WHITE:
 				white++
-			} else if c == cell.BLACK {
+			case cell.BLACK:
 				black++
 			}
 		}
