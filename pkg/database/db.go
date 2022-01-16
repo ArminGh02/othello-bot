573691edb217330450a01a04750e7608c8082f64
ArminGh02 <arminghorbanian02@gmail.com>
2022-01-16 20:05:39 +0330

    Add helper struct `PlayerDoc` and its constructor

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -8,6 +8,26 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type PlayerDoc struct {
+	UserID             int64
+	Name               string
+	Wins               int
+	Losses             int
+	Draws              int
+	LegalMovesAreShown bool
+}
+
+func newPlayerDoc(userID int64, name string, wins, losses, draws int, legalMovesAreShown bool) *PlayerDoc {
+	return &PlayerDoc{
+		UserID:             userID,
+		Name:               name,
+		Wins:               wins,
+		Losses:             losses,
+		Draws:              draws,
+		LegalMovesAreShown: legalMovesAreShown,
+	}
+}
+
 type DBHandler struct {
 	client *mongo.Client
 	coll   *mongo.Collection
