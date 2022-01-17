d66ddceca2de86d14b5723d2abb6ad6da02622ac
ArminGh02 <arminghorbanian02@gmail.com>
2022-01-17 16:18:24 +0330

    Use godotenv to load .env file

--- cmd/othellobot/main.go
+++ cmd/othellobot/main.go
@@ -7,9 +7,15 @@ import (
 	"time"
 
 	"github.com/ArminGh02/othello-bot/pkg/othellobot"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+
 	token := os.Getenv("OTHELLO_TOKEN")
 	mongodbURI := os.Getenv("OTHELLO_MONGODB_URI")
 	if token == "" || mongodbURI == "" {
