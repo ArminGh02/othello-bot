9d4de80b1c0a3cf5ebf210514dee2229a8fb544c
ArminGh02 <arminghorbanian02@gmail.com>
2022-01-19 21:55:53 +0330

    Define `byScore` and `Scoreboard`

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,6 +1,11 @@
 package util
 
-import "github.com/ArminGh02/othello-bot/pkg/util/coord"
+import (
+	"sort"
+
+	"github.com/ArminGh02/othello-bot/pkg/database"
+	"github.com/ArminGh02/othello-bot/pkg/util/coord"
+)
 
 type CoordSet struct {
 	m map[coord.Coord]struct{}
@@ -29,4 +34,41 @@ func (set *CoordSet) Contains(c coord.Coord) bool {
 
 func (set *CoordSet) IsEmpty() bool {
 	return len(set.m) == 0
+}
+
+type byScore []database.PlayerDoc
+
+func (b byScore) Len() int {
+	return len(b)
+}
+
+func (b byScore) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b byScore) Less(i, j int) bool {
+	return b[i].Score() > b[j].Score()
+}
+
+type Scoreboard struct {
+	scoreboard []database.PlayerDoc
+}
+
+func NewScoreboard(players []database.PlayerDoc) Scoreboard {
+	sort.Sort(byScore(players))
+	return Scoreboard{
+		scoreboard: players,
+	}
+}
+
+func (s *Scoreboard) Insert(player *database.PlayerDoc) {
+
+}
+
+func (s *Scoreboard) UpdateRankOf(userID int64) {
+
+}
+
+func (s *Scoreboard) RankOf(userID int64) {
+
 }
