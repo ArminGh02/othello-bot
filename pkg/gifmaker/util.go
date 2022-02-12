4da75b9e9e31ef923ddc7de1fe7762f9678395d0
ArminGh02 <arminghorbanian02@gmail.com>
2022-02-12 18:40:40 +0330

    Change signature and defintion of `gifmaker.cloneImage`

--- pkg/gifmaker/util.go
+++ pkg/gifmaker/util.go
@@ -2,52 +2,17 @@ package gifmaker
 
 import (
 	"image"
+	"image/color/palette"
 	"image/draw"
+	"image/gif"
+	"image/png"
 	"log"
+	"os"
 )
 
-func cloneImage(src image.Image) draw.Image {
-	switch s := src.(type) {
-	case *image.Alpha:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.Alpha16:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.Gray:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.Gray16:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.NRGBA:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.NRGBA64:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.RGBA:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	case *image.RGBA64:
-		clone := *s
-		clone.Pix = clonePix(s.Pix)
-		return &clone
-	default:
-		log.Panicf("gifmaker.cloneImage: unknown image.Image subclass: %T\n", s)
-		return nil  // although this statement is unreachable, Go forces me to write it!
-	}
-}
-
-func clonePix(b []uint8) []uint8 {
-	c := make([]uint8, len(b))
-	copy(c, b)
-	return c
+func cloneImage(src *image.Paletted) *image.Paletted {
+	clone := *src
+	clone.Pix = make([]uint8, len(src.Pix))
+	copy(clone.Pix, src.Pix)
+	return &clone
 }
