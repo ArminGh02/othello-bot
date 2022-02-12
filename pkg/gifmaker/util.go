871e4d37cdb50f980410129e7d670654ff96f3cf
ArminGh02 <arminghorbanian02@gmail.com>
2022-02-12 18:42:34 +0330

    Write `gifmaker.convertImageToPaletted`

--- pkg/gifmaker/util.go
+++ pkg/gifmaker/util.go
@@ -23,6 +23,17 @@ func readPNG(filename string) image.Image {
 	return img
 }
 
+func convertImageToPaletted(img image.Image) *image.Paletted {
+	opts := gif.Options{
+		NumColors: 256,
+		Drawer:    draw.FloydSteinberg,
+	}
+
+	res := image.NewPaletted(img.Bounds(), palette.Plan9[:opts.NumColors])
+	opts.Drawer.Draw(res, img.Bounds(), img, image.Point{})
+	return res
+}
+
 func cloneImage(src *image.Paletted) *image.Paletted {
 	clone := *src
 	clone.Pix = make([]uint8, len(src.Pix))
